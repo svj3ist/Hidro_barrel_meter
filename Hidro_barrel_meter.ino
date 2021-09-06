03ab37976a6ce6d526cddac892975a5a20626068
svj3ist <svj3ist@gmail.com>
2021-09-06 17:28:15 +0300

    Update Hidro_barrel_meter.ino
    add pressure sensor

--- Hidro_barrel_meter.ino
+++ Hidro_barrel_meter.ino
@@ -4,10 +4,10 @@ static byte mymac[] = { 0x38,0x09,0x56,0x25,0x60,0x60 };
 
 static byte myip[] = { 192,168,0,102 };
 
-byte Ethernet::buffer[1200];
+byte Ethernet::buffer[1000];
 BufferFiller bfill;
 
-int Pins[] = { A0,A1,A2,A3,A4,A5,2,3,4,5,6,7,8,9};
+int Pins[] = { 14,15,16,17,18,19,3,4,5,6,7,8,9};
 
 
 const char http_OK[] =
@@ -30,36 +30,78 @@ const char http_Unauthorized[] =
 
 void homePage()
 {
-  bool sensor0 = digitalRead(A0);
-  bool sensor1 = digitalRead(A1);
-  bool sensor2 = digitalRead(A2);
-  bool sensor3 = digitalRead(A3);
-  bool sensor4 = digitalRead(A4);
-  bool sensor5 = digitalRead(A5);
-  bool sensor6 = digitalRead(2);
-  bool sensor7 = digitalRead(3);
-  bool sensor8 = digitalRead(4);
-  bool sensor9 = digitalRead(5);
-  bool sensor10 = digitalRead(6);
-  bool sensor11 = digitalRead(7);
+  bool sensor0 = digitalRead(14);
+  bool sensor1 = digitalRead(15);
+  bool sensor2 = digitalRead(16);
+  bool sensor3 = digitalRead(17);
+  bool sensor4 = digitalRead(18);
+  bool sensor5 = digitalRead(19);
+  bool sensor6 = digitalRead(8);
+  bool sensor7 = digitalRead(9);
+  bool sensor8 = digitalRead(3);
+  bool sensor9 = digitalRead(4);
+  bool sensor10 = digitalRead(5);
+  bool sensor11 = digitalRead(6);
+  bool sensor12 = digitalRead(7);
 
+Serial.print("barrel 1 = ");
+Serial.print(!sensor0);
+Serial.print(!sensor1);
+Serial.println(!sensor2);
 
+Serial.print("barrel 2 = ");
+Serial.print(!sensor4);
+Serial.print(!sensor5);
+Serial.println(!sensor6);
 
+Serial.print("barrel 3 = ");
+Serial.print(!sensor7);
+Serial.print(!sensor8);
+Serial.println(!sensor9);
+
+Serial.print("barrel 4 = ");
+Serial.print(!sensor10);
+Serial.print(!sensor11);
+Serial.println(!sensor12);
+
+  Serial.print("Pressure is:  ");
+  float pressure_kPa = map(analogRead(A6), 95, 922, 101, 1200);
+  Serial.print(pressure_kPa);
+  int kPa = pressure_kPa;
+  Serial.print(" kPa ");
+  Serial.print(" ");
+  float pressure_psi = pressure_kPa * 0.14503773773020923;
+  Serial.print(pressure_psi);
+  int psi = pressure_psi;
+  Serial.println(" psi ");
+
+    Serial.print("Pressure2 is: ");
+  float pressure2_kPa = map(analogRead(A7), 100, 922, 101, 1200);
+  Serial.print(pressure2_kPa);
+  int kPa2 = pressure2_kPa;
+  Serial.print(" kPa2");
+  Serial.print(" ");
+  float pressure2_psi = pressure2_kPa * 0.14503773773020923;
+  Serial.print(pressure2_psi);
+  int psi2 = pressure2_psi;
+  Serial.println(" psi2");
+  Serial.println(" ");
   bfill.emit_p(PSTR(
     "<title>Barrel Meter</title>" 
   "<meta http-equiv='refresh' content='1'/>")); 
 
 
  
-  bfill.emit_p(PSTR("<h1>barrel 1 = $D$D$D""</h1>"),sensor0, sensor1 ,sensor2 );
-  bfill.emit_p(PSTR("<h1>barrel 2 = $D$D$D""</h1>"),sensor3, sensor4 ,sensor5 );
-  bfill.emit_p(PSTR("<h1>barrel 3 = $D$D$D""</h1>"),sensor6, sensor7 ,sensor8 );
-  bfill.emit_p(PSTR("<h1>barrel 4 = $D$D$D""</h1>"),sensor9, sensor10 ,sensor11 );
-
-
+  bfill.emit_p(PSTR("<h1>barrel 1 = $D$D$D""</h1>"),!sensor0, !sensor1 ,!sensor2 );
+  bfill.emit_p(PSTR("<h1>barrel 2 = $D$D$D""</h1>"),!sensor3, !sensor4 ,!sensor5 );
+  bfill.emit_p(PSTR("<h1>barrel 3 = $D$D$D""</h1>"),!sensor6, !sensor7 ,!sensor8 );
+  bfill.emit_p(PSTR("<h1>barrel 4 = $D$D$D""</h1>"),!sensor9, !sensor10 ,!sensor11 );
+  bfill.emit_p(PSTR("<h1>Pressure kPa = $D""  Pressure psi = $D""</h1>"),kPa, psi );
+  bfill.emit_p(PSTR("<h1>Pressure2 kPa = $D""  Pressure2 psi = $D""</h1>"),kPa2, psi2 );
 }
 
 void setup() {
+      Serial.begin(9600);
   if (ether.begin(sizeof Ethernet::buffer, mymac,10) == 0);
 
   if (!ether.dhcpSetup());
@@ -67,12 +109,12 @@ void setup() {
 
  for(int i = 0; i <= 12; i ++)
   {
-pinMode(Pins[i],INPUT);
+pinMode(Pins[i],INPUT_PULLUP);
 }
 }
 
 void loop() {
- bool sensor0 = digitalRead(A0);
+
 
 
 
